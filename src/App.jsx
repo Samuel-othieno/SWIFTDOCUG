7ca14f8e4a2167a9d4de9032c08b7cc1b51eb226
Samuel Othieno <douglasothieno@gmail.com>
2023-12-17 22:05:31 +0300

    Add Tailwind UI Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import Nav from './Components/NavBar/NavBar';
+import HeroSection from './Components/HeroSection/Hero';
+import Contact from './Components/Contact/ContactSection';
+import Newsletter from './Components/NewsLetter/Newsletter Sections';
+import Blogs from './Components/Blogs/Blogsection';
+import Teams from './Components/Teams/Teamsection';
 
 function App() {
   return (
     <MantineProvider>
-
+       <Nav/>
+       <HeroSection/>
+       <Blogs/>
+       <Teams/>
+       <Contact/>
+       <Newsletter/>
     </MantineProvider>
   )
 }
